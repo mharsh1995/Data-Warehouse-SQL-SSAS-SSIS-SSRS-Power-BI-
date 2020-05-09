b34dbeb7b8270fe5240bea923dbfe0b9f80dac9e
mharsh1995 <47004894+mharsh1995@users.noreply.github.com>
2020-05-08 23:50:19 -0400

    Add files via upload

---
+++ Practice Scripts/SQLQuery1.sql
@@ -0,0 +1,39 @@
+select ASCII('T') as Number
+
+select CHAR(100) as Character
+
+SELECT CHARINDEX('C', 'TechOnTheNet.com') as Number
+
+select CONCAT('Harsh','Mehta') as String
+
+select 'Harsh'+'Mehta' as String
+
+select DATALENGTH('Harsh Mehta') as DataLength
+
+select LEFT('Harsh Mehta',5) as Answer
+
+select LEN('Harsh Mehta') as L
+
+select LOWER('HARSH MEHTA') as LowerCase
+
+select LTRIM('   Harsh    Mehta') as Trim
+
+select NCHAR(116) as Character
+
+SELECT PATINDEX('%nthe%', 'TechOnTheNet.com') as I
+
+SELECT REPLACE('Harsh Mehta', 'R', '4') as Change
+
+SELECT RIGHT('Harsh Mehta', 4) as R
+
+select RTRIM('Harsh    Mehta    ') as Character
+
+select SPACE(4) as S
+
+SELECT STR(123.456, 7, 3)
+
+SELECT STUFF('TechOnTheNet.com', 1, 5, 'CheckYourMath')
+
+SELECT SUBSTRING('TechOnTheNet.com', 1, 4)
+
+SELECT UPPER('harsh mehta')
